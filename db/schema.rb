c91f2cb35afe6485bcd9a819cf6073f6393e6443
thecookieorg <marko@thecookie.org>
2016-06-22 16:37:20 -0700

    added questions crud, added user_id to questions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160622231610) do
+ActiveRecord::Schema.define(version: 20160622233256) do
+
+  create_table "questions", force: :cascade do |t|
+    t.string   "name"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
+
+  add_index "questions", ["user_id"], name: "index_questions_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
